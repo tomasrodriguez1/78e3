5f22e8454f1023983f3fd11ff34251cc2c86ecef
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2023-11-14 19:20:24 -0300

    dsda

--- data/carga_datos.php
+++ data/carga_datos.php
@@ -1,7 +1,7 @@
 <?php
 require("data/conexion.php"); // Asegúrate de que este archivo contiene la conexión a tu base de datos.
 
-$csv_proveedores = file("data/CSV PAR/proveedores.csv");
+$csv_proveedores = file("CSV PAR/proveedores.csv");
 foreach ($csv_proveedores as $linea) {
     $linea = str_getcsv($linea, ",");
     $sql = "INSERT INTO proveedores (id, nombre, plataforma) VALUES ('$linea[0]', '$linea[1]', '$linea[2]')";
