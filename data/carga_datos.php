e714fbd07452c8e182795cd85b37604f32e7b1dc
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2023-11-14 19:44:35 -0300

    asd

--- data/carga_datos.php
+++ data/carga_datos.php
@@ -6,7 +6,7 @@ require("conexion.php"); // Asegúrate de que este archivo contiene la conexión
 
 $csv_proveedores = file("CSV PAR/proveedores.csv");
 foreach ($csv_proveedores as $linea) {
-    $linea = str_getcsv($linea, ",");
+    $linea = str_getcsv($linea, ";");
     $sql = "INSERT INTO proveedores (id, nombre, plataforma) VALUES ('$linea[0]', '$linea[1]', '$linea[2]')";
 
     if (!mysqli_query($conexion, $sql)) {
