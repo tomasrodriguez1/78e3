7915edfe5075347eea87fd3fa6b884470d5d8fd2
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2023-11-13 23:39:39 -0300

    ingreso de pagina pagina_principal

---
+++ pagina_principal.php
@@ -0,0 +1,29 @@
+<?php 
+include('./templates/header.html'); 
+require("data/conexion.php");
+
+session_start();
+
+// Verificar si el usuario ya está logueado
+if (!isset($_SESSION['user_id'])) {
+    header('Location: index.php'); // Redirigir al login si no está logueado
+    exit;
+}
+
+?>
+
+<body>
+
+    <!-- Barra de Navegación -->
+    <div class="navbar">
+        <a href="perfil_usuario.php">Mi Perfil</a>
+        <a href="pagina_suscripciones.php">Página de Suscripciones</a>
+        <a href="one_time_purchase.php">One Time Purchases</a>
+        <a href="consulta_inestructurada.php">Consulta Inestructurada</a>
+        <!-- Agrega aquí más enlaces según necesites -->
+    </div>
+
+    <!-- Aquí puedes incluir el resto del contenido de tu página -->
+
+</body>
+</html>
