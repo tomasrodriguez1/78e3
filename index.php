0978b2b4b6e9aed478785d4e0c39fe0d38e23fee
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2023-11-13 23:05:06 -0300

    p 2

--- index.php
+++ index.php
@@ -1,99 +1,66 @@
-<?php include('./templates/header.html'); ?>
+<?php 
+include('./templates/header.html'); 
+require("config/conexion.php");
+
+session_start();
+
+// Verificar si el usuario ya está logueado
+if (isset($_SESSION['user_id'])) {
+    header('Location: pagina_principal.php'); // Redirigir a la página principal
+    exit;
+}
+
+$error = '';
+if (isset($_SESSION['error'])) {
+    $error = $_SESSION['error'];
+    unset($_SESSION['error']);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Consulta preparada para evitar inyecciones SQL
+    $sql = "SELECT * FROM Usuarios WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if ($row && password_verify($password, $row['password'])) {
+        // Usuario autenticado correctamente
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $username;
+
+        // Redirigir a otra página, por ejemplo, un perfil de usuario
+        header("Location: pagina_principal.php");
+        exit;
+    } else {
+        // Credenciales incorrectas
+        $_SESSION['error'] = "Credenciales incorrectas.";
+        header("Location: index.php");
+        exit;
+    }
+}
+?>
 
 <body>
     <h1 align="center"> BIBLIOTECA DE VIDEOJUEGOS Y PELICULAS</h1>
-    <p style="text-align:center;"> En esta plataforma podrás encontrar todas las películas y juegos que desees en distintios proveedores</p>
+    <p style="text-align:center;">En esta plataforma podrás encontrar todas las películas y juegos que desees en distintos proveedores</p>
 
-    <br>
+    <?php if ($error): ?>
+        <p style="color:red; text-align:center;"><?php echo $error; ?></p>
+    <?php endif; ?>
 
-    <h3 align="center">Aprieta este boton para ver los proveedores que tienen todos los videojuegos</h3>
-
-    <form action="./consultas/consulta_juegos.php" method="post">
-        <input type="submit" value="Mostrar Juegos y Proveedores">
-    </form>
-
-    <br>
-    <br>
-
-    <h3 align="center">Ingresa el numero de reseñas positivas y encontraras los juegos con esa cantidad o mas de reseñas positivas</h3>
-
-    <form action="./consultas/consulta_resenas.php" method="post">
-        <label for="numero_resenas">Ingrese el número de reseñas positivas:</label>
-        <input type="text" id="numero_resenas" name="numero_resenas">
-        <input type="submit" value="Buscar Juegos">
-    </form>
-
-    <br>
-    <br>
-
-    <h3 align="center">Ingresa el juego que quieras y aparecerá los proveedores que tiene</h3>
-
-    <form action="./consultas/consulta_proveedores.php" method="post">
-        <label for="nombre_juego">Ingrese el nombre del juego:</label>
-        <input type="text" id="nombre_juego" name="nombre_juego">
-        <input type="submit" value="Buscar Proveedores">
-    </form>
-
-    <br>
-    <br>
-
-    <h3 align="center">Aprieta el genero que quieras buscar y aparecerán los juegos que tiene</h3>
-
-    <?php
-    # Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require("config/conexion.php");
-
-    # Se construye la consulta para obtener todos los géneros distintos
-    $query_generos = "SELECT DISTINCT nombre AS nombre_genero FROM Videojuego_genero;";
-    $result_generos = $db->prepare($query_generos);
-    $result_generos->execute();
-    $generos = $result_generos->fetchAll();
-    ?>
-
-    <form action="./consultas/consulta_genero.php" method="post">
-        <label for="genero">Seleccione un género:</label>
-        <select id="genero" name="genero">
-            <?php
-            foreach ($generos as $genero) {
-                echo "<option value=\"$genero[nombre_genero]\">$genero[nombre_genero]</option>";
-            }
-            ?>
-        </select>
-        <input type="submit" value="Buscar">
-    </form>
-
-    <br>
-    <br>
-
-    <h3 align="center">Ingresa el nombre de usuario a buscar y te aparecerán los juegos que tiene</h3>
-
-    <form action="./consultas/consulta_usuario_juego.php" method="post">
-        <label for="username">Ingrese el nombre de usuario:</label>
-        <input type="text" id="username" name="username">
-        <input type="submit" value="Buscar Juegos y Proveedores">
-    </form>
-
-    <br>
-    <br>
-
-    <h3 align="center">Ingresa el nombre de usuario a buscar y te aparecerá sus preordenes</h3>
-
-    <form action="./consultas/consulta_usuario_preorden.php" method="post">
-        <label for="username2">Ingrese el nombre de usuario:</label>
-        <input type="text" id="username2" name="username2">
-        <input type="submit" value="Buscar Proveedores con Preordenes">
-    </form>
-
-    <br>
-    <br>
-
-    <h3 align="center">Aprieta este boton para ver el gasto total de todos los usuarios en juegos por subscripción</h3>
-
-    <form action="./consultas/consulta_gasto_subscripcion.php" method="get">
-        <input type="submit" value="Ver gasto total de cada usuario por subscripción">
-    </form>
-
-    <br>
-    <br>
+    <div align="center">
+        <form method="post" action="index.php">
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" required><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Iniciar Sesión">
+        </form>
+    </div>
 </body>
 </html>
