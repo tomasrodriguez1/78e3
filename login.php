bd91e2f4d156e1a48c1cfb1bc78f4749e84b5966
Jose Aravena <josearavenasabugo@uc.cl>
2023-11-13 22:17:26 -0300

    v1

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+# Busqueda del usuario en la base de datos
+$sql = "SELECT * FROM Usuarios WHERE username = '$username'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+
+
+# Encriptar la contraseña y ver si coincide con la encriptacion de la base de datos
+if ($row && password_verify($password, $row['password'])) {
+    // Usuario autenticado correctamente
+
+    // Mostrar la información desde una vista materializada
+    $sql = "SELECT * FROM VistaInformacionUsuario WHERE username = '$username'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+
+    // Mostrar la información en la página Mi Perfil
+    echo "Nombre: " . $row['nombre'] . "<br>";
+    echo "Email: " . $row['email'] . "<br>";
+    // ...
+
+} else {
+    // Credenciales incorrectas, mostrar mensaje de error o redirigir a página de inicio de sesión
+    echo "Credenciales incorrectas.";
+}
+?>
