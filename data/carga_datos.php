645bf9b125cff8d468d29029c0555dc60772dbf6
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2023-11-14 23:54:56 -0300

    asd

--- data/carga_datos.php
+++ data/carga_datos.php
@@ -4,103 +4,90 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require("conexion.php");
 
-function eliminarBOM($valor) {
-    if (substr($valor, 0, 3) == pack('CCC', 0xef, 0xbb, 0xbf)) {
-        $valor = substr($valor, 3);
+function verificarCampos($linea, $indicesCampos) {
+    foreach ($indicesCampos as $indice) {
+        if (!isset($linea[$indice]) || trim($linea[$indice]) === '') {
+            return false;
+        }
     }
-    return $valor;
+    return true;
 }
+
 try {
-    // Iniciar transacción para proveedores
     $db->beginTransaction();
 
     $csv_proveedores = file("CSV PAR/proveedores.csv");
     foreach ($csv_proveedores as $index => $linea) {
-        if ($index === 0) { // Saltar el encabezado
-            continue;
-        }
+        if ($index === 0) continue;
 
         $linea = str_getcsv($linea, ";");
-        $idLimpio = filter_var(trim($linea[0]), FILTER_SANITIZE_NUMBER_INT);
-
-        // Verificar si los campos críticos están vacíos
-        if (!is_numeric($idLimpio) || trim($linea[1]) == '' || trim($linea[2]) == '') {
-            echo "Advertencia: Línea con datos insuficientes en la línea " . ($index + 1) . ". Esta línea será omitida.\n";
+        if (!verificarCampos($linea, [0, 1, 2])) {
+            echo "Línea omitida en proveedores: " . ($index + 1) . "\n";
             continue;
         }
 
-        // Verificar si el proveedor ya existe
+        $idLimpio = filter_var(trim($linea[0]), FILTER_SANITIZE_NUMBER_INT);
         $sqlVerificar = "SELECT COUNT(*) FROM proveedores WHERE id = :id OR nombre = :nombre";
         $stmtVerificar = $db->prepare($sqlVerificar);
         $stmtVerificar->bindParam(':id', $idLimpio, PDO::PARAM_INT);
         $stmtVerificar->bindParam(':nombre', $linea[1]);
         $stmtVerificar->execute();
+
         if ($stmtVerificar->fetchColumn() > 0) {
             echo "Proveedor ya cargado: " . $linea[1] . "\n";
             continue;
         }
 
-        // Insertar el proveedor si no existe
         $sql = "INSERT INTO proveedores (id, nombre, plataforma) VALUES (:id, :nombre, :plataforma)";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':id', $idLimpio, PDO::PARAM_INT);
         $stmt->bindParam(':nombre', $linea[1]);
         $stmt->bindParam(':plataforma', $linea[2]);
         $stmt->execute();
+
         echo "Datos cargados proveedores\n";
     }
 
-    // Confirmar la transacción para proveedores
     $db->commit();
 } catch (Exception $e) {
-    // Revertir la transacción si hay un error
     $db->rollBack();
     echo "Error durante la carga de datos de proveedores: " . $e->getMessage();
 }
 
-
 try {
-    // Iniciar transacción para genero_subgenero
     $db->beginTransaction();
 
-    $csv_genero_subgenero = file("CSV PAR/genero_subgenero.csv");
+    $csv_genero_subgenero = file("CSV PAR/genero.csv");
     foreach ($csv_genero_subgenero as $linea) {
         $linea = str_getcsv($linea, ";");
-
-        // Verificar si el campo subgenero está vacío
-        if (trim($linea[1]) == '') {
-            echo "Advertencia: Subgénero vacío para el género " . $linea[0] . ". Esta línea será omitida.\n";
+        if (!verificarCampos($linea, [0, 1])) {
+            echo "Línea omitida en género/subgénero: " . ($index + 1) . "\n";
             continue;
         }
 
-        // Verificar si el genero_subgenero ya existe
         $sqlVerificar = "SELECT COUNT(*) FROM genero_subgenero WHERE genero = :genero AND subgenero = :subgenero";
         $stmtVerificar = $db->prepare($sqlVerificar);
         $stmtVerificar->bindParam(':genero', $linea[0]);
         $stmtVerificar->bindParam(':subgenero', $linea[1]);
         $stmtVerificar->execute();
+
         if ($stmtVerificar->fetchColumn() > 0) {
             echo "Genero y subgenero ya cargados: " . $linea[0] . ", " . $linea[1] . "\n";
             continue;
         }
 
-        // Insertar genero_subgenero si no existe
         $sql = "INSERT INTO genero_subgenero (genero, subgenero) VALUES (:genero, :subgenero)";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':genero', $linea[0]);
         $stmt->bindParam(':subgenero', $linea[1]);
         $stmt->execute();
+
         echo "Datos cargados genero_subgenero\n";
     }
 
-    // Confirmar la transacción para genero_subgenero
     $db->commit();
 } catch (Exception $e) {
-    // Revertir la transacción si hay un error
     $db->rollBack();
     echo "Error durante la carga de datos de genero_subgenero: " . $e->getMessage();
 }
-
-
-
 ?>
