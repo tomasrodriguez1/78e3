2f55896ca508e2097421c7496a64b055c70dbea4
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2023-11-23 18:40:37 -0300

    asdad

--- pagina_principal.php
+++ pagina_principal.php
@@ -15,22 +15,42 @@ if (!isset($_SESSION['user_id'])) {
     header('Location: index.php'); // Redirigir al login si no está logueado
     exit;
 }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Aquí tu código para procesar el formulario y realizar la consulta
-    // Por ejemplo:
-    // $resultados = tuFuncionParaConsultarLaBaseDeDatos($_POST['proveedor'], $_POST['nombre_videojuego']);
+    $proveedor = '%' . $_POST['proveedor'] . '%';
+    $nombre = '%' . $_POST['nombre_videojuego'] . '%';
 
-    // Mostrar Resultados de la Búsqueda
-    foreach ($resultados as $row) {
-        // Mostrar cada resultado
-        echo "<div>";
-        echo "Proveedor: " . htmlspecialchars($row['nombre_proveedor']);
-        echo " - Videojuego: " . htmlspecialchars($row['nombre_videojuego']);
-        echo " - Precio: " . htmlspecialchars($row['precio']);
-        echo "</div>";
+    try {
+        $sql = "SELECT p.nombre AS nombre_proveedor, 'Película' AS tipo, peli.titulo AS titulo
+                FROM Proveedores AS p
+                LEFT JOIN ProveedoresPeliculas AS pp ON p.id = pp.pro_id
+                LEFT JOIN Peliculas AS peli ON pp.pid = peli.pid
+                WHERE LOWER(p.nombre) LIKE LOWER(:proveedor)
+                AND LOWER(peli.titulo) LIKE LOWER(:nombre)
+                UNION
+                SELECT p.nombre, 'Serie', ser.nombre AS titulo
+                FROM Proveedores AS p
+                LEFT JOIN ProveedoresSeries AS ps ON p.id = ps.pro_id
+                LEFT JOIN Series AS ser ON ps.sid = ser.sid
+                WHERE LOWER(p.nombre) LIKE LOWER(:proveedor)
+                AND LOWER(ser.nombre) LIKE LOWER(:nombre)";
+        $stmt = $db2->prepare($sql);
+        $stmt->bindParam(':proveedor', $proveedor, PDO::PARAM_STR);
+        $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+        $stmt->execute();
+        $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($resultados as $row) {
+            echo "<div>";
+            echo "Proveedor: " . htmlspecialchars($row['nombre_proveedor']);
+            echo " - Tipo: " . htmlspecialchars($row['tipo']);
+            echo " - Título: " . htmlspecialchars($row['titulo']);
+            echo "</div>";
         }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
+}
+
 ?>
 
 <body>
@@ -62,13 +82,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     <!-- Procesar el formulario y obtener resultados -->
-    <!-- FALTA ADAPTARLO A LA BASE DE DATOS DE GRUPO IMPAR -->
     <h4 align="center"> Suscripciones de Películas y Series</h4>
     <br>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="formulario-busqueda">
         <div class="inputs-container">
             <input type="text" name="proveedor" placeholder="Nombre del Proveedor">
-            <input type="text" name="nombre_videojuego" placeholder="Nombre del Videojuego">
+            <input type="text" name="nombre_videojuego" placeholder="Nombre de la Serie o Película">
         </div>
         <input type="submit" class="btn-logout" value="Buscar">
     </form>
@@ -77,20 +96,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <h4 align="center"> Proveedores de Películas y Series</h4>
     <div class="proveedores-container">
         <?php
-        try {
-            $sql = "SELECT * FROM Proveedores";
-            $stmt = $db2->query($sql);
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo "<div class='proveedor-bloque' data-id='" . htmlspecialchars($row['id']) . "' data-nombre='" . htmlspecialchars($row['nombre']) . "' data-costo='" . htmlspecialchars($row['costo']) . "'>" . htmlspecialchars($row['nombre']) . "</div>";
+            try {
+                $sql = "SELECT Proveedores.id, Proveedores.nombre, Proveedores.costo, 
+                            (SELECT COUNT(*) FROM ProveedoresPeliculas WHERE ProveedoresPeliculas.pro_id = Proveedores.id) as totalpeliculas,
+                            (SELECT COUNT(*) FROM ProveedoresSeries WHERE ProveedoresSeries.pro_id = Proveedores.id) as totalseries
+                        FROM Proveedores";
+                $stmt = $db2->query($sql);
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<div class='proveedor-bloque' data-id='" . htmlspecialchars($row['id']) . "' data-nombre='" . htmlspecialchars($row['nombre']) . "' data-costo='" . htmlspecialchars($row['costo']) . "' data-totalpeliculas='" . htmlspecialchars($row['totalpeliculas']) . "' data-totalseries='" . htmlspecialchars($row['totalseries']) . "'>" . htmlspecialchars($row['nombre']) . "</div>";
+                }
+            } catch (Exception $e) {
+                echo "Error: " . $e->getMessage();
             }
-        } catch (Exception $e) {
-            echo "Error: " . $e->getMessage();
-        }
         ?>
+
     </div>
-
-
-
     <br>
     <br>
     <br>
@@ -101,38 +121,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="logout-button">
         <a href="auth/logout.php" class="btn-logout">Cerrar Sesión</a>
     </div>
-<!-- FIN DE LA PAGINA -->
+    <!-- FIN DE LA PAGINA -->
 
-<!-- Modal para detalles del proveedor -->
-<div id="modalProveedor" style="display:none;">
-    <div id="detallesProveedor"></div>
-    <br>
-    <button onclick="cerrarModal()">Cerrar</button>
-</div>
+    <!-- Modal para detalles del proveedor -->
+    <div id="modalProveedor" style="display:none;">
+        <div id="detallesProveedor"></div>
+        <br>
+        <button onclick="cerrarModal()">Cerrar</button>
+    </div>
 
-<!-- Código JavaScript -->
-<script>
-    document.addEventListener('DOMContentLoaded', (event) => {
-        document.querySelectorAll('.proveedor-bloque').forEach(item => {
-            item.addEventListener('click', function(e) {
-                var proveedorId = this.getAttribute('data-id');
-                var proveedorNombre = this.getAttribute('data-nombre');
-                var proveedorCosto = this.getAttribute('data-costo');
-                
-                var detallesHTML = "<h3>" + proveedorNombre + "</h3>";
-                detallesHTML += "<p>ID: " + proveedorId + "</p>";
-                detallesHTML += "<p>Costo: $" + proveedorCosto + "</p>";
-                
-                document.getElementById('detallesProveedor').innerHTML = detallesHTML;
-                document.getElementById('modalProveedor').style.display = 'block';
+    <!-- Código JavaScript -->
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            document.querySelectorAll('.proveedor-bloque').forEach(item => {
+                item.addEventListener('click', function(e) {
+                    var proveedorId = this.getAttribute('data-id');
+                    var proveedorNombre = this.getAttribute('data-nombre');
+                    var proveedorCosto = this.getAttribute('data-costo');
+                    var totalPeliculas = this.getAttribute('data-totalpeliculas');
+                    var totalSeries = this.getAttribute('data-totalseries');
+                    
+                    var detallesHTML = "<h3>" + proveedorNombre + "</h3>";
+                    detallesHTML += "<p>Costo: $" + proveedorCosto + "</p>";
+                    detallesHTML += "<p>Total de Películas: " + totalPeliculas + "</p>";
+                    detallesHTML += "<p>Total de Series: " + totalSeries + "</p>";
+                    
+                    document.getElementById('detallesProveedor').innerHTML = detallesHTML;
+                    document.getElementById('modalProveedor').style.display = 'block';
+                });
             });
         });
-    });
 
-    function cerrarModal() {
-        document.getElementById('modalProveedor').style.display = 'none';
-    }
-</script>
+        function cerrarModal() {
+            document.getElementById('modalProveedor').style.display = 'none';
+        }
+    </script>
+
 
 
 
