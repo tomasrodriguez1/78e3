e1183926594c528391f214bb04c6ff3d7f719132
Jose Aravena <josearavenasabugo@uc.cl>
2023-11-23 21:47:26 -0300

    one_time

---
+++ paginas/confirmacion_compra_videojuego.php
@@ -0,0 +1,17 @@
+<?php
+require("../data/conexion.php");
+include('../templates/header.html');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Confirmación de Compra</title>
+</head>
+<body>
+    <h1>¡Felicitaciones, tu compra se ha realizado con éxito!</h1>
+    <a href="../index.php">Volver al inicio</a>
+</body>
+</html>
